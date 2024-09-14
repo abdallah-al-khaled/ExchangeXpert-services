15aa27e8030a9cadea72c30e84c139b3a3e903ea
Abdallah <fafafavd@gmail.com>
2024-09-14 08:43:14 +0300

    Read nasdaq csv file

--- Trading Bots/Sentiment_analysis_bot.py
+++ Trading Bots/Sentiment_analysis_bot.py
@@ -5,3 +5,7 @@ import websockets
 import pandas as pd
 import requests
 from transformers import BertTokenizer, BertForSequenceClassification, pipeline
+
+df = pd.read_csv('nasdaq.csv')
+stock_symbols = set(df['Symbol'].tolist())  # Use a set for fast lookup
+print(stock_symbols)
