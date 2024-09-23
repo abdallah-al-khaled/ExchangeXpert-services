42ebfdfbd0682d5faae2287396f4ecaeeb070cda
Abdallah <fafafavd@gmail.com>
2024-09-23 23:49:43 +0300

    Added FinBert sentiment analysis model

--- Sentiment Analysis/sentiment-analysis.py
+++ Sentiment Analysis/sentiment-analysis.py
@@ -4,3 +4,7 @@ from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import pipeline
 import torch
 import pandas as pd
+
+# Load the FinBERT sentiment analysis model
+tokenizer = BertTokenizer.from_pretrained('ProsusAI/finbert')
+model = BertForSequenceClassification.from_pretrained('ProsusAI/finbert')
