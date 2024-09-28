437e6ada4f6e5813d08ec08642cf69b64416ba8d
Abdallah <fafafavd@gmail.com>
2024-09-28 22:35:17 +0300

    Initialized stock symbols array

--- Stocks ML/run_forecast.py
+++ Stocks ML/run_forecast.py
@@ -3,3 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly
+
+stock_symbols = [
+    'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'NVDA', 'meta', 'JPM', 'JNJ', 'V',
+    'PG', 'DIS', 'MA', 'UNH', 'HD', 'PYPL', 'BAC', 'NFLX', 'XOM', 'VZ',
+    'KO', 'PEP', 'MRK', 'INTC', 'T', 'PFE', 'CSCO', 'WMT', 'BA', 'NKE',
+    'GS', 'MCD', 'ADBE', 'COST', 'IBM', 'CRM', 'ORCL', 'WFC', 'MDT', 'C',
+]
