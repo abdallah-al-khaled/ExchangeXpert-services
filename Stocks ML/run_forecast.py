bd082852cf1c2edd2a17a6d57972cdf4ff84acbe
Abdallah <fafafavd@gmail.com>
2024-09-28 22:37:10 +0300

    Added train function for the prophet model

--- Stocks ML/run_forecast.py
+++ Stocks ML/run_forecast.py
@@ -18,3 +18,26 @@ def fetch_stock_data(symbol, start_date='2015-01-01'):
     # Prophet requires columns as 'ds' and 'y'
     stock_data = data[['Date', 'Close']].rename(columns={'Date': 'ds', 'Close': 'y'})
     return stock_data
+
+# Configure and train Prophet model for each stock
+def train_prophet(stock_data, stock_name, yearly_seasonality=True, daily_seasonality=False, holidays=None, changepoint_prior_scale=0.05):
+    # Initialize Prophet with customized parameters
+    m = Prophet(yearly_seasonality=yearly_seasonality,
+                    daily_seasonality=daily_seasonality,
+                    holidays=holidays,
+                    changepoint_prior_scale=changepoint_prior_scale)
+
+    # Fit the model to stock data
+    m.fit(stock_data)
+
+    # Forecast for the next 365 days
+    future = m.make_future_dataframe(periods=365)
+    forecast = m.predict(future)
+
+    # Plot forecast
+    fig = m.plot(forecast)
+    plt.title(f'Forecast for {stock_name}')
+    plt.show()
+
+    # Return the forecast for further analysis
+    return forecast
